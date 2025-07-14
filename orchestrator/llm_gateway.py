88d1c30dcd862567d0f26aea44921ba7c60a042f
lynn <hajjwlynn@gmail.com>
2025-07-14 14:12:52 +0300

    Ignore models folder and update LLM gateway

--- orchestrator/llm_gateway.py
+++ orchestrator/llm_gateway.py
@@ -1,6 +1,6 @@
 from llama_cpp import Llama
 
-llm = Llama(model_path="models\deepseek-llm-7b-chat.Q4_K_M.gguf", n_ctx=2048)
+llm = Llama(model_path="models\capybarahermes-2.5-mistral-7b.Q4_K_M.gguf", n_ctx=2048)
 
 def call_llm(prompt):
     response = llm.create_chat_completion(
